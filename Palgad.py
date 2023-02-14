5e567b975f392841350a7552e337d071f3a93d18
matveikulakovskiii <120181570+matveikulakovskiii@users.noreply.github.com>
2023-02-15 01:36:06 +0200

    Add files via upload

--- Palgad.py
+++ Palgad.py
@@ -4,7 +4,7 @@ inimesed=["A","B","C","D","f"]
 while True:
     print(inimesed)
     print(palgad)
-    menu=int(input("Valik:\n 1-lisa andmed\n2=Kustuta andmed\n3=Suurim palk\n4=Lühim palk\n5=Sorteerimine\n6=Võrdsed palgad\n13=Kustuta\n"))
+    menu=int(input("Valik:\n 1-lisa andmed\n2=Kustuta andmed\n3=Suurim palk\n4=Lühim palk\n5=Sorteerimine\n6=Võrdsed palgad\n7-otsi palka inimese nime järgi\n8-sisestatud palga põhjal kuvab inimeste madalamad ja kõrgemad palgad\n9-Leia top 3 vähem või rohkem\n10-Otsi keskmine palk ja kes on saada suurem\n11-Otsi maksuvaba palk\n12-Sort\n13-Otsige kes teenivad alla keskmise palga ja emalda nad\n14-Teeb nimekiri\n15-saate teada 5% tõusuga töötaja töötasu T-aastas\n16-Vaheta nimi kolmandale\n17 Boonus\n"))
     if menu==0:
         break 
     elif menu==1:
@@ -30,13 +30,30 @@ while True:
         print(inimesed)
         print(palgad)
     elif menu==7:
-        inimesed,palgad=lepani(inimesed,palgad)
-        print(inimesed)
-        print(palgad)
-    elif
+        iniemsed,palgad=imja(inimesed,palgad)
+    elif menu==8:
+        amount = int(input("Sisestage summa, millega võrrelda: "))
+        more_than = disp(palgad, inimesed, amount, "suurim")
+        less_than = disp(palgad, inimesed, amount, "vähem")
+        print(f"Inimesed, kellel on palk rohkem kui {amount}:", more_than)
+        print(f"Inimesed, kelle palk on väiksem kui {amount}:", less_than)
+    elif menu==9:
+        top(inimesed,palgad)
+    elif menu==10:
+        kesk(inimesed,palgad)
+    elif menu==11:
+        tulu(inimesed,palgad)
     elif menu==12:
-        palgad,inimesed=Sorteerimine(palgad,inimesed)
-        print(inimesed,palgad)
+        inimesed,palgad=sort(inimesed,palgad)
     elif menu==13:
         inimesed,palgad=Kustuta(inimesed,palgad)
-        print(inimesed,palgad)
+    elif menu==14:
+        inimesed,palgad=tint(inimesed,palgad)
+    elif menu==15:
+        inimesed,palgad=year(inimesed,palgad)
+    elif menu==16:
+        inimesed=vahetanimikolmandale(inimesed)
+    elif menu==17:
+        inimesed,palgad=boonus(inimesed,palgad)
+    else:
+        print("Kirjuta õige arv")
--- module1.py
+++ module1.py
@@ -90,49 +90,152 @@ def vordsed_palgad(i:list,p:list):
             nimi=i[k]
             print(nimi)
 
-def lepani(i:list,p:list):
-    nimi=input("Kelle palk tahad leia? ").title()
+def imja(i:list,p:list):
+    nimi=input("Kelle palk tahad leia? \n")
     while nimi not in i:
-        nimi = input("Palun kirjutage õige nimi: ").title
+        nimi=input("Palun kirjuta õige nimi \n")
     n=i.count(nimi)
     if n!=1:
-        print(f"Siis in mõned inimesed kes nimi on {nimi}")
+        print(f'Siin on mõned inimesed kes nimi on {nimi} \n')
         kopia=i.copy()
-        for i in range(n):
+        for j in range(n):
             ind=kopia.index(nimi)
             kopia.remove(nimi)
-            kopia.insert(ind,"")
-            print(f"{i+1} {nimi} saab {p[ind]}")
+            kopia.insert(ind,'')
+            print(f'{j+1} {nimi} saab {p[ind]} \n')
     else:
         ind=i.index(nimi)
-        print(f"{nimi} saab {p[ind]}")
-
-def Sorteerimine(i:list,p:list):
-
-    v=int(input("palk 1-kahaneb,2-kasvab? "))
-    if v==1:
-        n=len(p)
-        for j in range(0,n-1):
-            for k in range(j+1,n):
-                if p[j]>p[k]:
-                    abi=p[j]
-                    p[j]=p[k]
-                    p[k]=abi
-                    abi=i[j]
-                    i[j]=i[k]
-                    i[k]=abi
-    elif v==2:
-        n=len(p)
-        for j in range(0,n-1):
-            for k in range(j+1,n):
-                if p[j]<p[k]:
-                    abi=p[j]
-                    p[j]=p[k]
-                    p[k]=abi
-                    abi=i[j]
-                    i[j]=i[k]
-                    i[k]=abi
+        print(f'{nimi} saab {p[ind]} \n')
     return i,p
+
+def disp(palgad, inimesed, amount, compare):
+    result = []
+    for i in range(len(palgad)):
+        if compare == "suurim" and palgad[i] > amount:
+            result.append((inimesed[i], palgad[i]))
+        elif compare == "vähem" and palgad[i] < amount:
+            result.append((inimesed[i], palgad[i]))
+    return result
+
+def top(i:list,p:list): 
+    kopia=p.copy()
+    for j in range(3):
+        ind=kopia.index(min(kopia))
+        print(f"{i+1} inimene - {i[ind]} saab väikse palk: {p[ind]}")
+        kopia.pop(ind)
+        kopia.insert(ind,max(p)+1)
+    kopia=p.copy()
+    for j in range(3):
+        ind=kopia.index(max(kopia))
+        print(f"{j+1} inimene - {i[ind]} saab suur palk: {p[ind]}")
+        kopia.pop(ind)
+        kopia.insert(ind,min(p)+1)
+
+def kesk(i:list,p:list): 
+    kesk=sum(p)/len(p)
+    print(f"Keskmine palk on {kesk}")
+    for j in range(len(i)):
+        if p[j]>=kesk:
+            print(f"{i[j]} saab suurem kui keskmine palk, ta saab {p[j]}")
+
+def tulu(i:list,p:list): 
+    for j in range(0,len(p)):
+        if i[j]<500:
+            palk=p[j]
+        elif 500>=p[i]<=1200:
+            palk=(p[i]-500)-(p[j]-500)*0.2
+        elif 1200>p[i]>=2099:
+            palk=(500-(5/9)*(p[j]-1200))-(500-(5/9)*(p[j]-1200))*0.2
+        else:
+            palk=p[j]*0.2
+        print(f"{i[j]} on maksuvaba palk {palk}")
+
+def sort(i:list,p:list):
+    vali=input("Sorteeri nime (1) või palga järgi (2) ")
+    while vali not in ["1","2"]:
+        vali=input("Kirjuta ainult 1 või 2 ")
+    if vali=="1":
+        vali_2=input("A–Z või Z–A ").upper()
+        while vali_2 not in ["A-Z","Z-A"]:
+            vali_2=input("A–Z või Z–A ").upper()
+        for l in range(0,len(i)):
+            for o in range(0,len(i)):
+                if i[l]==i[o] and l!=o:
+                    for u in range(0,len(i)):
+                        i[o]+=f"_{u+1}"
+                        break
+        kopia=i.copy()
+        i.sort()
+        p1=[]
+        for j in range(0,len(i)):
+            ind=kopia.index(i[j])
+            p1.insert(j,p[ind])
+        p=p1
+        if vali_2=="Z-A":
+            i.reverse()
+            p.reverse()
+    else:
+        for l in range(0,len(i)):
+            for o in range(0,len(i)):
+                if p[l]<p[o]:
+                    abi=p[l]
+                    p[l]=p[o]
+                    p[o]=abi
+                    abi=i[l]
+                    i[l]=i[o]
+                    i[o]=abi
+        vali_2=input("Kasvav või kahanev järjekord ").title()
+        while vali_2 not in ["Kasvav","Kahanev"]:
+            vali_2=input("Kasvav või kahanev! ").title()
+        if vali_2=="Kahanev":
+            i.reverse()
+            p.reverse()
+    return i,p 
+
+def Kustuta(i:list,p:list):
+    kesk_palk=sum(p)/len(p)
+    print(kesk_palk)
+    v=int(input("palk 1-suurem,2-väiksem? "))
+    if v==1:
+        for palk in p:
+            if palk>kesk_palk:
+                ind=p.index(palk)
+                p.remove(palk)
+                i.pop(ind)
+    else:
+        for palk in p:
+            if palk<kesk_palk:
+                ind=p.index(palk)
+                p.remove(palk)
+                i.pop(ind)
+    return i,p
+
+def tint(i:list,p:list):
+
+    for j in range(0,len(i)):
+        i[j]=i[j].title()
+        p[j]=round(p[j],1) 
+        p[j]=int(p[j])
+    return i,p
+
+def year(i:list,p:list):
+    T = int(input('Mitu aastat? '))
+    for j in range(len(p)):
+        p[j] = p[j] * 1.05 ** T
+    return i,p
+
+def vahetanimikolmandale(lst):
+    for i in range(2, len(lst), 3):
+        uusnimi = input(f"Millise nimele vahetada {lst[i]}? ")
+        lst[i] = uusnimi
+    return lst
+
+def boonus(i:list,p:list):
+    T = int(input('Palju boonus on? '))
+    for j in range(len(p)):
+        p[j] = p[j] + T
+    return i,p
+
 
 def Kustuta(i:list,p:list):
     kesk_palk=sum(p)/len(p)
